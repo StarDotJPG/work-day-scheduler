82b04230c3c7644aba96d9b6cd18adaf0ec2c406
Jason Greenia <jason.greenia@gmail.com>
2022-01-30 13:22:30 -0800

    added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # work-day-scheduler-challenge
+
+## Purpose
+A simple calendar website that allows a user to save events for each hour of the day
+
+## Built with
+* HTML
+* CSS using Bootstrap and Font Awesome
+* JavaScript using jQuery and Momentjs
+
+## Website
+https://stardotjpg.github.io/work-day-scheduler-challenge/
+
+## Contribution
+Made with ❤️ by Jason Greenia
+
+## Website Screenshot
+![Website Screenshot](/assets/img/screenshot.png?raw=true)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,21 @@ var tasks = [];
 
 var displayDay = function () {
 
+    // set the current day at the top of the page
     var currentDay = moment().format("dddd MMMM D, YYYY");
     $("#currentDay").html(currentDay);
 
     var hourToDisplay = moment("8AM", "ha");
     var thisHour = moment().startOf('hour');
 
-    // this creates the initial day schedule
+    // this creates the day schedule
     for (var i = 0; i < 10; i++) {
         // dynamically create each timeblock row
         var timeblock = $("<div>").addClass("row");
+        // below line adds the hour of the time block to the html text as well as the class name, so we can find it easier later
         var hour = $("<div>").addClass("col-1 hour " + hourToDisplay.format("ha")).html(hourToDisplay.format("ha"));
         var task = $("<div>").addClass("col description").append($("<p>"));
+        // below line uses font awesome lock icon and bootstrap to center it in the div
         var save = $("<div>").addClass("col-1 saveBtn d-flex align-items-center justify-content-center").append($("<i>").addClass("fas fa-lock"));
 
         // add styling for past/present/future
@@ -35,6 +38,7 @@ var displayDay = function () {
         hourToDisplay.add(1, 'hours');
     }
 
+    // load any tasks saved in local storage
     loadTasks();
 }
 
@@ -46,7 +50,7 @@ var loadTasks = function () {
     // then finds the class that matches the value of the timeblock property
     // then looks for the corresponding description p and sets it to the value of the description property
     for (var i = 0; i < tasks.length; i++) {
-       $("." + tasks[i].timeblock).parent().children(".description").children("p").text(tasks[i].description);       
+        $("." + tasks[i].timeblock).parent().children(".description").children("p").text(tasks[i].description);
     };
 };
 
@@ -71,7 +75,7 @@ $(".container").on("click", ".description", function () {
 
 // save button was clicked
 $(".container").on("click", ".saveBtn", function () {
-    // we need to raverse the DOM to get the value of textarea and the time value
+    // we need to traverse the DOM to get the value of textarea and the time value
     var text = $(this).parent().children(".description").children("textarea").val();
     var time = $(this).parent().children(".hour").text();
 
